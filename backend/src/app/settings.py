05cb7748ded438a57ccdf49cb194fb2237a2bb30
Mustapha-Moumanis <moumanis.mustapha@gmail.com>
2025-10-23 13:13:03 +0100

    add csrf to the env file

--- backend/src/app/settings.py
+++ backend/src/app/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
-# CSRF_TRUSTED_ORIGINS = env.list("ALLOWED_HOSTS", default=[])
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 # Application definition
 
